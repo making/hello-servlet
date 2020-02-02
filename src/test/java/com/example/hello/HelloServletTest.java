b9069665fb6636e636a692317ff1c3a9e3f3fe0d
Toshiaki Maki <tmaki@pivotal.io>
2020-02-02 13:28:47 +0900

    Update

--- src/test/java/com/example/hello/HelloServletTest.java
+++ src/test/java/com/example/hello/HelloServletTest.java
@@ -27,7 +27,7 @@ public class HelloServletTest {
 
 
 		assertThat(res.getContentType(), equalTo("text/plain;charset=utf-8"));
-		assertThat(writer.toString(), equalTo("Hello World V4!!!"));
+		assertThat(writer.toString(), equalTo("Hello World!"));
 	}
 
 	static class MockHttpServletResponse implements HttpServletResponse {
